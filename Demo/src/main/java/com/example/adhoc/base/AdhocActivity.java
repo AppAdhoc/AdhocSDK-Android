9c8d3a68ac85dba32cb101118c4a7007ff5388ed
dong yuangui <dongyuangui@dong-work-2.local>
2015-12-14 16:03:40 +0800

    去掉自动统计

--- Demo/src/main/java/com/example/adhoc/base/AdhocActivity.java
+++ Demo/src/main/java/com/example/adhoc/base/AdhocActivity.java
@@ -1,20 +1,19 @@
 package com.example.adhoc.base;
 
 import android.support.v7.app.ActionBarActivity;
-import android.view.MotionEvent;
 
 import com.adhoc.adhocsdk.AdhocTracker;
 
 
 public class AdhocActivity extends ActionBarActivity {
 
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent event) {
-
-        AdhocTracker.autoTracking(this, event);
-
-        return super.dispatchTouchEvent(event);
-    }
+//    @Override
+//    public boolean dispatchTouchEvent(MotionEvent event) {
+//
+//        AdhocTracker.autoTracking(this, event);
+//
+//        return super.dispatchTouchEvent(event);
+//    }
 
     @Override
     protected void onResume() {
